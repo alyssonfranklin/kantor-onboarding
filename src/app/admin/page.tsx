1755f1b29275f728c1aa2b080a13a8e2880d0482
Alysson Franklin <alyssonfranklin@BeloHorizonteAlyssonFranklin.local>
2025-02-27 19:13:19 -0600

    Additional Look & feel fixes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,7 +3,7 @@ import AgentOrgAdminCreation from '@/components/AgentOrgAdminCreation';
 
 export default function AdminPage() {
   return (
-    <main className="container min-h-screen bg-gray-800 py-8 px-4">
+    <main className="container mx-auto py-8 px-4">
       <AgentOrgAdminCreation />
     </main>
   );
